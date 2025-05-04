d903b93e80182ad0f40be70fc047994224521b64
akeenahall42029 <hallake@DESKTOP-K462FL7.>
2025-05-04 16:12:00 +0200

    Created barMetricsLog.csv for recording all metrics and ran 100+ tests. However, changing the implementation for the throughput metric so this file will be outdated and the tests will be reran with more columns for throughput (1st, 2nd, 3rd, etc quadrants

--- OS2_Assignment2025_barSim/scripts/run_tests.sh
+++ OS2_Assignment2025_barSim/scripts/run_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Test 1 parameters
-PATRONS=10 
-q=5
-s=5
-seed=40 #randomly picked randomizer number 
+PATRONS_LIST=(10 20 30 40 50 60 75 100 )
+q_LIST=(5 6 10 15  30 50 75 5)
+s_LIST=(2 3 4  5  6  10  25  5 )
+SEED_LIST=(40 45 55 60 65 65 100 100) #randomly picked randomizer number
 
 echo "=== Recompiling Project ==="
 javac -d bin src/barScheduling/*.java
@@ -13,27 +13,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Running test 1 with:"
-echo "- Patrons: $PATRONS"
-echo "-Quantum (q): $Q" 
-echo "-Context Switch (s): $s"
-echo "Seed (randomizer) $seed" 
-echo "-------------------------" 
 
+for i in "${!PATRONS_LIST[@]}"; do
+  PATRONS=${PATRONS_LIST[$i]}
+  Q=${q_LIST[$i]}
+  S=${s_LIST[$i]}
+  SEED=${SEED_LIST[$i]}
 
-#Test FCFS (sched = 0)
-echo "Testing FCFS..." 
-java -cp ./bin barScheduling.SchedulingSimulation $PATRONS 0 0 $s $seed > ./test_results/FCFS_results.txt
-#doesn't need a q value
+  echo "Running tests for P=$PATRONS | Q=$Q | S=$S | Seed=$SEED"
 
-#Test SJF (sched = 1)
-echo "Testing SJF..."
-java -cp ./bin barScheduling.SchedulingSimulation $PATRONS 1 0 $s $seed > ./test_results/SJF_results.txt
-#doesn't need a q value
+  # FCFS (sched=0)
+  java -cp bin barScheduling.SchedulingSimulation $PATRONS 0 $S 0  $SEED > "test_results/FCFS_results.txt"
 
-#Test RR (sched = 2)
-echo "Testing RR..."
-java -cp ./bin barScheduling.SchedulingSimulation $PATRONS 2 5 $s $seed > ./test_results/RR_results.txt
+  # SJF (sched=1)
+  java -cp bin barScheduling.SchedulingSimulation $PATRONS 1 $S 0 $SEED > "test_results/SJF_results.txt"
 
-echo "------------------------"
-echo "Tests completed. Results saved" 
+  # RR (sched=2)
+  java -cp bin barScheduling.SchedulingSimulation $PATRONS 2 $S $Q $SEED > "test_results/RR_results.txt"
+
+  echo "Done with P=$PATRONS"
+done
+
+echo  "All tests completed."
